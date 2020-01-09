608e4af0e48fef1a4239a8620b9ba62594d33345
Luke Stephens (hakluke) <13975395+hakluke@users.noreply.github.com>
2020-01-10 00:46:04 +1000

    Update README.md

--- main.go
+++ main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"sync"
 	"bufio"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
-
-func worker(url string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	resp, err := http.Get(url)
-	if err != nil{
-		return
-	}
-	fmt.Println(resp.StatusCode, url)
-}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	maxFileDescriptors, err := strconv.Atoi(strings.TrimSuffix(string(out), "\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var wg sync.WaitGroup
+	lock := make(chan struct{}, maxFileDescriptors)
+	client := &http.Client{Transport: &http.Transport{
+		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
+		Dial:                (&net.Dialer{Timeout: 0, KeepAlive: 0}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}}
+
 	for scanner.Scan() {
-		go worker(scanner.Text(), &wg)
 		wg.Add(1)
+		lock <- struct{}{}
+		go func(url string) {
+			defer wg.Done()
+			defer func() { <-lock }()
+
+			req, err := http.NewRequest("GET", url, nil)
+			req.Header.Set("Connection", "close")
+			if err != nil {
+				fmt.Println(-1, err)
+				return
+			}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(-1, err, url)
+				return
+			}
+			resp.Body.Close()
+			fmt.Println(resp.StatusCode, url)
+		}(scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
