3bbd1be97077fdf62fa3ecc2b4871d896b7edbf4
Вострых Михаил Анатольевич <MAVostrykh@domclick.ru>
2020-01-10 19:32:26 +0300

    fix resp.Body.Close(), add ulimit -n

--- main.go
+++ main.go
@@ -8,33 +8,38 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 	"sync"
+	"syscall"
 	"time"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+func ulimit() (uint64, error) {
+	var rLimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		return 0, err
+	}
+	return rLimit.Cur, nil
+}
 
-	out, err := exec.Command("ulimit", "-n").Output()
+func main() {
+	maxFileDescriptors, err := ulimit()
 	if err != nil {
 		log.Fatal(err)
 	}
-	maxFileDescriptors, err := strconv.Atoi(strings.TrimSuffix(string(out), "\n"))
-	if err != nil {
-		log.Fatal(err)
+	if maxFileDescriptors-100 < 0 {
+		log.Fatalf("maxFileDescriptors==%d is not enough", maxFileDescriptors)
 	}
 
 	var wg sync.WaitGroup
-	lock := make(chan struct{}, maxFileDescriptors)
+	lock := make(chan struct{}, maxFileDescriptors-100)
 	client := &http.Client{Transport: &http.Transport{
 		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
 		Dial:                (&net.Dialer{Timeout: 0, KeepAlive: 0}).Dial,
 		TLSHandshakeTimeout: 5 * time.Second,
 	}}
 
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		wg.Add(1)
 		lock <- struct{}{}
@@ -45,12 +50,12 @@ func main() {
 			req, err := http.NewRequest("GET", url, nil)
 			req.Header.Set("Connection", "close")
 			if err != nil {
-				fmt.Println(-1, err)
+				fmt.Println(999, err)
 				return
 			}
 			resp, err := client.Do(req)
 			if err != nil {
-				fmt.Println(-1, err, url)
+				fmt.Println(999, err, url)
 				return
 			}
 			resp.Body.Close()
